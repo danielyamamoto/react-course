b233731eed09b1172de4f2e3ba915a8029a8da14
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-03-22 12:07:40 -0600

    Setup's BurgerApp game

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,11 @@
 import React, { Component } from "react";
-import classes from "../assets/css/style.css";
-import Validation from "../components/Validation/Validation";
-import Char from "../components/Char/Char";
 
 class App extends Component {
-    state = {
-        userInput: "",
-    };
-
-    inputChangedHandler = (event) => {
-        this.setState({ userInput: event.target.value });
-    };
-
-    deleteCharHandler = (index) => {
-        const text = this.state.userInput.split("");
-        text.splice(index, 1);
-        const updatedText = text.join("");
-        this.setState({ userInput: updatedText });
-    };
-
     render() {
-        const charList = this.state.userInput.split("").map((ch, index) => {
-            return (
-                <Char
-                    character={ch}
-                    key={index}
-                    clicked={() => this.deleteCharHandler(index)}
-                />
-            );
-        });
-
         return (
-            <div className={classes.App}>
-                <input
-                    type="text"
-                    onChange={this.inputChangedHandler}
-                    value={this.state.userInput}
-                />
-                <p>{this.state.userInput}</p>
-                <Validation inputLength={this.state.userInput.length} />
-                {charList}
-            </div>
+            <div>
+
+            </div>    
         );
     }
 }
