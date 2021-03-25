85a2afbc89e3d533728cd0807c2387e1bac99602
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-03-25 16:27:15 -0600

    Reset React:(

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from '../../../assets/css/style.css';
-
-const backdrop = props => (
-    props.show ? <div className={classes.backdrop} onClick={props.clicked}></div> : null
-);
-
-export default backdrop;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/normalize.css'
-import './assets/css/style.css'
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
