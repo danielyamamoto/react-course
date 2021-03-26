01e590ede6d936f774a729d7f52f470052c1b622
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-03-26 13:42:21 -0600

    Improve performance

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.scss';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = props => {
     const attachedClasses = props.open ? 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from "./App.module.scss";
-import Layout from '../components/Layout/Layout';
+import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
