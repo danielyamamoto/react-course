977e841e76c219fd26e7e6403a1b3526e13dd03e
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-04-04 00:35:18 -0600

    Getting orders by user account | sorting on firebase db

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,7 +87,8 @@ class ContactReact extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
 
         this.props.onOrderBurger(order, this.props.token);
@@ -138,7 +139,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     };
 };
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -25,6 +25,7 @@ export const purchaseBurgerStart = ()  => {
 export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
+
         axios.post('/orders.json?auth=' + token, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData.data));
@@ -61,11 +62,13 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = token => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
 
-        axios.get('/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+
+        axios.get('/orders.json' + queryParams)
             .then(res => {
                 const fetchOrders = [];
                 for(let key in res.data) {
