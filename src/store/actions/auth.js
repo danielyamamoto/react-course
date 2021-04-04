5ebf5ffc636730746ace9fad2357d8c79cf0101c
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-04-03 23:14:26 -0600

    Persistence user's data | localstorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFailed = error => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -52,6 +55,10 @@ export const auth = (email, password, isSignup) => {
 
         axios.post(url, authData)
             .then(res => {
+                const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
+                localStorage.setItem('token', res.data.idToken);
+                localStorage.setItem('expirationTime', expirationDate);
+                localStorage.setItem('userId', res.data.localId);
                 dispatch(authSuccess(res.data.idToken, res.data.localId));
                 dispatch(checkAuthTimeout(res.data.expiresIn));
             })
@@ -65,5 +72,25 @@ export const setAuthRedirectPath = path => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        }
+        else {
+            const expirationTime = new Date(localStorage.getItem('expirationTime'));
+            if (expirationTime <= new Date()) {
+                dispatch(logout());
+            }
+            else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationTime.getTime() - new Date().getTime()) / 1000));
+            }
+        }
     };
 };
